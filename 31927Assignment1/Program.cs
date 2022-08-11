3f9220b0006131db1d660e6cb2e69ddea09dc491
Melvin <melvinhkusuma@gmail.com>
2022-08-11 16:22:50 +1000

    cleaned some code, masked password input

--- 31927Assignment1/Program.cs
+++ 31927Assignment1/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace _31927Assignment1
 {
     class Program
     {
-        const int width = 70; //width of table
+        const int width = 50; //width of table
         static int originX;
         static int originY;
         static List<(int, int)> pos = new List<(int, int)>(); //store cursor positions for input fields when creating tables
@@ -30,8 +31,6 @@ namespace _31927Assignment1
                 }
                 switch (MainMenu())
                 {
-                    case 0:
-                        break;
                     case 1:
                         CreateAccountMenu();
                         break;
@@ -52,7 +51,7 @@ namespace _31927Assignment1
             }
         }
 
-        static void Table(string title, string subtitle = "", string content = "") //draw table
+        static void Table(string title, string subtitle = "", string content = "") //dynamically draw tables
         {
             pos.Clear(); //clear any previously stored positions
             Console.Clear(); //clear any previous display
@@ -67,13 +66,12 @@ namespace _31927Assignment1
                 Console.WriteLine($"║{CentreText(subtitle)}║");
                 Console.WriteLine($"║{new String(' ', width)}║");
             }
-
             int x; //initialise cursor pos for input fields
             int y = string.IsNullOrWhiteSpace(subtitle) ? 3 : 5; //start at 5 if there's a subtitle, otherwise 3
             foreach (string line in File.ReadLines(content))
             {
                 string text = line;
-                if (!string.IsNullOrWhiteSpace(line) && line[line.Length-1] == '|') //check if the line is an input field
+                if (!string.IsNullOrWhiteSpace(line) && line[line.Length - 1] == '|') //check line is an input field
                 {
                     text = line.Remove(line.Length - 1); //remove delimiter
                     x = line.Length + 1;
@@ -82,7 +80,6 @@ namespace _31927Assignment1
                 Console.WriteLine($"║{text}{new String(' ', width - text.Length)}║");
                 y++;
             }
-
             Console.WriteLine($"╚{border}╝");
         }
 
@@ -110,21 +107,46 @@ namespace _31927Assignment1
 
         static void WriteErrorMsg((int, int) pos, string msg)
         {
-            Console.SetCursorPosition(pos.Item1, pos.Item2 + 1);
-            Console.WriteLine(msg);
+            Console.SetCursorPosition(pos.Item1 + 1, pos.Item2 + 1);
+            Console.WriteLine(CentreText(msg));
         }
 
         static void LoginMenu()
         {
             Table("Bank Account Management Console", "Login", "MenuTemplates/LoginMenu.txt");
             (int, int) errorMsgPos = Console.GetCursorPosition(); //store cursor pos for error message, which at this time is below the table
+            ResizeWindow(errorMsgPos);
             string[] credentials = new string[2];
             while (true)
             {
                 for (int i = 0; i < pos.Count; i++)
                 {
                     Console.SetCursorPosition(originX + pos[i].Item1, originY + pos[i].Item2);
-                    credentials[i] = Console.ReadLine();
+                    if (i == 1)
+                    {
+                        StringBuilder pw = new StringBuilder();
+                        while (true)
+                        {
+                            var key = Console.ReadKey(true);
+                            if (key.Key == ConsoleKey.Enter) 
+                                break;
+                            if (key.Key == ConsoleKey.Backspace && pw.Length > 0)
+                            {
+                                pw.Remove(pw.Length - 1, 1);
+                                Console.CursorLeft--;
+                                Console.Write(' ');
+                                Console.CursorLeft--;
+                            }
+                            else if (key.Key != ConsoleKey.Backspace)
+                            {
+                                Console.Write("*");
+                                pw.Append(key.KeyChar);
+                            }
+                        }
+                        credentials[i] = pw.ToString();
+                    } 
+                    else
+                        credentials[i] = Console.ReadLine();
                 }
                 foreach (string line in File.ReadLines("Storage/Login.txt"))
                 {
@@ -134,6 +156,7 @@ namespace _31927Assignment1
                         return;
                     }
                 }
+                credentials = new string[2];
                 ClearAllFields(pos);
                 WriteErrorMsg(errorMsgPos, "Incorrect username or password.");
             }
@@ -143,17 +166,16 @@ namespace _31927Assignment1
         {
             Table("Main Menu", "Options", "MenuTemplates/MainMenu.txt");
             (int, int) errorMsgPos = Console.GetCursorPosition();
+            ResizeWindow(errorMsgPos);
             string choice;
             while (true)
             {
                 Console.SetCursorPosition(originX + pos[0].Item1, originY + pos[0].Item2);
                 choice = Console.ReadLine();
-                if (!int.TryParse(choice, out _) || !(Int32.Parse(choice) >= 1 && Int32.Parse(choice) <= 7))
+                if (!int.TryParse(choice, out _) || 1 > Int32.Parse(choice) || Int32.Parse(choice) > 7)
                 {
-                    {
-                        ClearAllFields(pos);
-                        WriteErrorMsg(errorMsgPos, "Invalid option; please input a number between 1-7.");
-                    }
+                    ClearAllFields(pos);
+                    WriteErrorMsg(errorMsgPos, "Invalid option; input a number between 1-7.");
                 }
                 else
                     break;
@@ -163,12 +185,28 @@ namespace _31927Assignment1
 
         static void CreateAccountMenu()
         {
-            string[] credentials = new string[5];
             Table("Create New Account", "Enter details", "MenuTemplates/NewAccountMenu.txt");
+            (int, int) errorMsgPos = Console.GetCursorPosition();
+            ResizeWindow(errorMsgPos);
+            string[] credentials = new string[5];
             for (int i = 0; i < pos.Count; i++)
             {
                 Console.SetCursorPosition(originX + pos[i].Item1, originY + pos[i].Item2);
                 credentials[i] = Console.ReadLine();
+            }
+        }
+
+        static void ResizeWindow((int, int) pos) //resize window according to table height and width
+        {
+            try
+            {
+                Console.WindowHeight = pos.Item2 + 3;
+                Console.WindowWidth = width + 3;
+                Console.SetCursorPosition(originX, originY); //scroll to top
+            }
+            catch (System.NotSupportedException) //WindowHeight/Width incompatible on OSX
+            {
+                return;
             }
         }
     }
