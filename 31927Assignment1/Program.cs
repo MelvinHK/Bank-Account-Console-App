fff7105e2c4b1a5281bfbb7b598b20fbc678a716
Melvin <melvinhkusuma@gmail.com>
2022-08-11 13:36:37 +1000

    optimised code and memory usage by removing recursive calls on menus, making the whole thing iterative.

--- 31927Assignment1/Program.cs
+++ 31927Assignment1/Program.cs
@@ -20,7 +20,36 @@ namespace _31927Assignment1
             Console.Clear();
             originX = Console.CursorLeft;
             originY = Console.CursorTop;
-            LoginMenu();
+            bool login = true;
+            while (true)
+            {
+                if (login)
+                {
+                    LoginMenu();
+                    login = false;
+                }
+                switch (MainMenu())
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        CreateAccountMenu();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        login = true;
+                        break;
+                }
+            }
         }
 
         static void Table(string title, string subtitle = "", string content = "") //draw table
@@ -39,12 +68,12 @@ namespace _31927Assignment1
                 Console.WriteLine($"║{new String(' ', width)}║");
             }
 
-            int x; //initialise cursor positions for input fields
+            int x; //initialise cursor pos for input fields
             int y = string.IsNullOrWhiteSpace(subtitle) ? 3 : 5; //start at 5 if there's a subtitle, otherwise 3
             foreach (string line in File.ReadLines(content))
             {
                 string text = line;
-                if (line.Contains('|')) //check if the line is an input field
+                if (!string.IsNullOrWhiteSpace(line) && line[line.Length-1] == '|') //check if the line is an input field
                 {
                     text = line.Remove(line.Length - 1); //remove delimiter
                     x = line.Length + 1;
@@ -64,66 +93,72 @@ namespace _31927Assignment1
                                  text, ""); //create padding before and after text
         }
 
-        static void LoginMenu(string err = "")
+        static void ClearAllFields(List<(int, int)> pos) //clears input fields
         {
-            string[] credentials = new string[2];
-            Table("Bank Account Management Console", "Login", "MenuTemplates/LoginMenu.txt");
-            if (!string.IsNullOrEmpty(err))
-                Console.WriteLine("\n" + err);
             for (int i = 0; i < pos.Count; i++)
             {
-                Console.SetCursorPosition(originX + pos[i].Item1, originY + pos[i].Item2);
-                credentials[i] = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(credentials[i]))
-                {
-                    if (i == 0)
-                        LoginMenu("The username field is required.");
-                    else
-                        LoginMenu("The password field is required.");
-                    return;
-                }
+                Console.SetCursorPosition(originX + pos[i].Item1, originY + pos[i].Item2); //set cursor pos to input fields
+                Console.Write(new String(' ', width - pos[i].Item1)); //replace up to the width of the table with whitespace
             }
-            foreach (string line in File.ReadLines("Storage/Login.txt"))
-            {
-                string[] check = line.Split('|');
-                if (check[0].Trim().ToLower().Equals(credentials[0].ToLower()) && check[1].Trim().Equals(credentials[1]))
-                {
-                    MainMenu();
-                    return;
-                }
-            }
-            LoginMenu("Incorrect username or password.");
         }
 
-        static void MainMenu(string err = "")
+        static void ClearField((int, int) pos) //clears an input field
+        {
+            Console.SetCursorPosition(originX + pos.Item1, originY + pos.Item2);
+            Console.Write(new String(' ', width - pos.Item1));
+        }
+
+        static void WriteErrorMsg((int, int) pos, string msg)
+        {
+            Console.SetCursorPosition(pos.Item1, pos.Item2 + 1);
+            Console.WriteLine(msg);
+        }
+
+        static void LoginMenu()
+        {
+            Table("Bank Account Management Console", "Login", "MenuTemplates/LoginMenu.txt");
+            (int, int) errorMsgPos = Console.GetCursorPosition(); //store cursor pos for error message, which at this time is below the table
+            string[] credentials = new string[2];
+            while (true)
+            {
+                for (int i = 0; i < pos.Count; i++)
+                {
+                    Console.SetCursorPosition(originX + pos[i].Item1, originY + pos[i].Item2);
+                    credentials[i] = Console.ReadLine();
+                }
+                foreach (string line in File.ReadLines("Storage/Login.txt"))
+                {
+                    string[] check = line.Split('|');
+                    if (check[0].Trim().ToLower().Equals(credentials[0].ToLower()) && check[1].Trim().Equals(credentials[1]))
+                    {
+                        return;
+                    }
+                }
+                ClearAllFields(pos);
+                WriteErrorMsg(errorMsgPos, "Incorrect username or password.");
+            }
+        }
+
+        static int MainMenu()
         {
             Table("Main Menu", "Options", "MenuTemplates/MainMenu.txt");
-            if (!string.IsNullOrEmpty(err))
-                Console.WriteLine("\n" + err);
-            Console.SetCursorPosition(originX + pos[0].Item1, originY + pos[0].Item2);
-            string choice = Console.ReadLine();
-            switch (choice)
+            (int, int) errorMsgPos = Console.GetCursorPosition();
+            string choice;
+            while (true)
             {
-                case "1":
-                    CreateAccountMenu();
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                case "5":
-                    break;
-                case "6":
-                    break;
-                case "7":
-                    LoginMenu();
-                    break;
-                default:
-                    MainMenu("Invalid option; please a input a number between 1 - 7.");
+                Console.SetCursorPosition(originX + pos[0].Item1, originY + pos[0].Item2);
+                choice = Console.ReadLine();
+                if (!int.TryParse(choice, out _) || !(Int32.Parse(choice) >= 1 && Int32.Parse(choice) <= 7))
+                {
+                    {
+                        ClearAllFields(pos);
+                        WriteErrorMsg(errorMsgPos, "Invalid option; please input a number between 1-7.");
+                    }
+                }
+                else
                     break;
             }
+            return Int32.Parse(choice);
         }
 
         static void CreateAccountMenu()
